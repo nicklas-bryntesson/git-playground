fb4dc3589f5d2e31dfea67317c6c00938175f8e7
Nicklas Bryntesson <nicklas.bryntesson@gmail.com>
2021-05-09 09:53:49 +0200

    Added wrapper around single posts content to target better with CSS grid

--- wp-content/themes/twentytwenty/singular.php
+++ wp-content/themes/twentytwenty/singular.php
@@ -13,20 +13,22 @@ get_header();
 ?>
 
 <main id="site-content" role="main">
+	<div class="post-wrapper">
+	
+		<?php
 
-	<?php
+		if ( have_posts() ) {
 
-	if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
 
-		while ( have_posts() ) {
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_type() );
+			}
 		}
-	}
 
-	?>
+		?>
 
+	</div>
 </main><!-- #site-content -->
 
 <?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
